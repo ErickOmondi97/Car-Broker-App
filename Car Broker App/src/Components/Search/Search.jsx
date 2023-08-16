1f78aaee2baa014996b72d67e6a4940f6339463f
Erick Omondi <eriqueasaria@gmail.com>
2023-08-16 21:04:16 +0300

    Added code for the Search  UI

--- Car Broker App/src/Components/Search/Search.jsx
+++ Car Broker App/src/Components/Search/Search.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import '../../css/App.css';
 
+//imported icons
+import {AiOutlineSearch} from 'react-icons/ai'
+
 const Search = () => {
   return (
-    <div className=''>
+    <div className='search'>
+      <div className="secContainer container">
+        <h3 className="title">
+          Which vehicle are you looking for?
+        </h3>
 
+        <div className="searchDiv grid">
+          <input type="text" placeholder='Type'/>
+          <input type="text" placeholder='Year'/>
+          <input type="text" placeholder='Model'/>
+          <input type="text" placeholder='Price'/>
+          <button className="btn primaryBtn flex">
+            <AiOutlineSearch className='icon'/>
+            <span>
+              Search
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
