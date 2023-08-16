63eb203977f3f60a78b42918ffffe462547151e4
Erick Omondi <eriqueasaria@gmail.com>
2023-08-16 20:56:41 +0300

    Added code for the home page UI

--- Car Broker App/src/Components/Home/Home.jsx
+++ Car Broker App/src/Components/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import '../../css/App.css';
 
+//Imported Images
+//import homeImage from '';
+
 const Home = () => {
   return (
     <div className='home'>
+      <div className="secContainer">
+        <div className="homeText">
+          <span className="homeSpan">Meet Your New Car</span>
+          <h1 className="homeTitle">Honda Civic Type R</h1>
+          <div className="btns flex">
+            <button className="btn">More Details</button>
+            <button className="btn primaryBtn">TestDrive</button>
+          </div>
+        </div>
 
+        <div className="homeImage">
+          <img src='' alt="Home Image" />
+        </div>
+      </div>
     </div>
   )
 }
