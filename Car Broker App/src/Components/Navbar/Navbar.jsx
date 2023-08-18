92590fd957720283040e1958d10296fafabb51ec
Erick Omondi <eriqueasaria@gmail.com>
2023-08-18 21:46:42 +0300

    I added the background to the navbar when the user scrolls

--- Car Broker App/src/Components/Navbar/Navbar.jsx
+++ Car Broker App/src/Components/Navbar/Navbar.jsx
@@ -22,8 +22,19 @@ const Navbar = () => {
   const removeNavbar = () => {
     setNavbar('navbar')
   }
+
+  //function to add a background to the Navbar when we scroll a certain height on the window
+  const [header, setHeader] = useState('header')
+  const addBg = () => {
+    if(window.scrollY >= 20){
+      setHeader('header addBg')
+    }
+  }
+
+  window.addEventListener ('scroll', addBg)
+
   return (
-    <div className='header'>
+    <div className={header}>
       <div className="logoDiv">
         <img src={logo} alt="Logo Image" className="logo"/>
       </div>
