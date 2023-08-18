0e31b8b2481e242427619ce4fa439372096140f1
Erick Omondi <eriqueasaria@gmail.com>
2023-08-18 21:53:29 +0300

    I added code for  the sellers' section but I am yet to complete it

--- Car Broker App/src/Components/Sellers/Sellers.jsx
+++ Car Broker App/src/Components/Sellers/Sellers.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import '../../css/App.css';
 
+//Imported Images
+
+
 const Sellers = () => {
   return (
-    <div className=''>
+    <div className='sellers section'>
+      <div className="secContainer container">
+        <div className="secHeading grid">
+          <h3 className="secTitle">
+            Explore top sellers in town
+          </h3>
+
+          <p>Joseph Samuel Girard holds the Guiness World Record for being the greatest salesman in the world</p>
+        </div>
+
+        <div className="sellersContainer">
+          {/* Single seller */}
+          <div className="singleSeller flex">
+            <div className="imgDiv flex">
+              <img src="" alt="" className="img"/>
+            </div>
+            <span className="info">
+              <h4 className="name">
+                Toyota
+              </h4>
+              <p>from 2M</p>
+            </span>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
