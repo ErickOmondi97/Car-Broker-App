e62c8777d269867459b7ad15a56f2ebb46277897
Erick Omondi <eriqueasaria@gmail.com>
2023-08-20 16:26:42 +0300

    I have added content in the review section

--- Car Broker App/src/Components/Review/Review.jsx
+++ Car Broker App/src/Components/Review/Review.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import '../../css/App.css';
 
+//import icons
+import {BsArrowLeftShort} from 'react-icons/bs';
+import {BsArrowRightShort} from 'react-icons/bs';
+import {AiFillStar} from 'react-icons/ai';
+
+
 const Review = () => {
   return (
     <div className='review section'>
@@ -11,6 +17,207 @@ const Review = () => {
           <div className="navBtns flex">
             <BsArrowLeftShort className='icon leftIcon' />
             <BsArrowRightShort className='icon rightIcon' />
+          </div>
+        </div>
+
+        {/* review container */}
+        <div className="reviewContainer grid">
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
+          </div>
+          
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
+          </div>
+          
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
+          </div>
+          
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
+          </div>
+          
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
+          </div>
+          
+          {/* single review */}
+          <div className="singleReview grid">
+            <div className="imgDiv">
+              <img src="" alt="" />
+            </div>
+
+            <h5 className="reviewTitle">
+              2023 Cadillac
+            </h5>
+            <span className="desc">
+              Good cars are safe,fast, affordavle to purchase, economical to operate, reliable, capacious, comfortable and attractive.
+            </span>
+            
+            <div className="reviewerDiv flex">
+              <div className="leftDiv flex">
+                <div className="reviewerImage">
+                  <img src="" alt="Reviewer Image" />
+                </div>
+                <div className="aboutReviewer">
+                  <span className="name">
+                    Milani Asaria
+                  </span>
+                  <p>Chief Editor</p>
+                </div>
+              </div>
+
+              <div className="rightDiv flex">
+                <AiFillStar className="icon" />
+                <blockquote>4.84</blockquote>
+              </div>
+            </div>
           </div>
         </div>
       </div>
