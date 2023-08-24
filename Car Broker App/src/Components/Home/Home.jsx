7584d34c08b8a8ea28edaa8270e74b30287be69c
Erick Omondi <eriqueasaria@gmail.com>
2023-08-24 21:31:12 +0300

    I added code for the animation feature

--- Car Broker App/src/Components/Home/Home.jsx
+++ Car Broker App/src/Components/Home/Home.jsx
@@ -4,20 +4,32 @@ import '../../css/App.css';
 //Imported Images
 import homeImage from '../../Assets/Honda-Civic-Type-R.png';
 
+//imported aos for animation and scroll reveal
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Home = () => {
+  //initialized Aos and set the default animation duration
+  useEffect (()=>{
+    Aos.init (
+      {
+        duration: 2000
+      }
+    )
+  }, [])
   return (
     <div className='home'>
-      <div className="secContainer">
+      <div data-aos='fade-up' className="secContainer">
         <div className="homeText">
           <span className="homeSpan">Meet Your New Car</span>
           <h1 className="homeTitle">Honda Civic Type R</h1>
           <div className="btns flex">
-            <button className="btn">More Details</button>
-            <button className="btn primaryBtn">TestDrive</button>
+            <button className="btn" data-aos='fade-right'>More Details</button>
+            <button data-aos='fade-left' className="btn primaryBtn">TestDrive</button>
           </div>
         </div>
 
-        <div className="homeImage">
+        <div data-aos='fade-down-right' className="homeImage">
           <img src={homeImage} alt="Home Image" />
         </div>
       </div>
