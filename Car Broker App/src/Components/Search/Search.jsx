1fb9f5aee6f51161260549d6d5d2f409992af8c9
Erick Omondi <eriqueasaria@gmail.com>
2023-08-24 21:32:30 +0300

    I added code for the animation feature

--- Car Broker App/src/Components/Search/Search.jsx
+++ Car Broker App/src/Components/Search/Search.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '../../css/App.css';
 
 //imported icons
 import {AiOutlineSearch} from 'react-icons/ai'
 
+//imported aos for animation and scroll reveal
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Search = () => {
+  //initialized Aos and set the default animation duration
+  useEffect (()=>{
+    Aos.init (
+      {
+        duration: 2000
+      }
+    )
+  }, [])
+
   return (
     <div className='search'>
       <div className="secContainer container">
-        <h3 className="title">
+        <h3 data-aos='fade-up' className="title">
           Which vehicle are you looking for?
         </h3>
 
         <div className="searchDiv grid">
-          <input type="text" placeholder='Type'/>
-          <input type="text" placeholder='Year'/>
-          <input type="text" placeholder='Model'/>
-          <input type="text" placeholder='Price'/>
+          <input data-aos='fade-up' type="text" placeholder='Type'/>
+          <input data-aos='fade-up'type="text" placeholder='Year'/>
+          <input data-aos='fade-up' type="text" placeholder='Model'/>
+          <input data-aos='fade-up' type="text" placeholder='Price'/>
           <button className="btn primaryBtn flex">
             <AiOutlineSearch className='icon'/>
             <span>
